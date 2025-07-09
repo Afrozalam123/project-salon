2ec293b5c2b28903d9c4d0ab14cbf55924093d7e
Afrozalam123 <alamafroz70400@gmail.com>
2025-07-10 01:07:21 +0530

    updated code and add new Components and updating Existing files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import image from "../assests/image3.jpg"
 import image4 from "../assests/image4.jpg"
 import hair  from "../assests/hair.jpg"
 import beard from "../assests/beard.jpg"
+import logoImg from "../assests/new-logo.png"
 // import { useNavigate } from 'react-router-dom';
 
 
@@ -14,6 +15,9 @@ function Home() {
   return (
     <div className='image-bg'>
     <img src={Photo} alt='' className='back-img'/>
+    <div>
+    <img src={logoImg} alt="salon logo" className='modern-logo'/>
+    </div>
     <div>
      <h1 id='heading1'>Welcome to Modern & stylish saloon</h1>
      <p id='para'    >Get your best look for Modern & Stylish saloon <br/>and enjoy personalized beauty care by experts <br/> and glow up with lastest trend and Modern in Hair,skin and grooming </p>
@@ -39,23 +43,42 @@ function Home() {
        
        <section className='home-cards'>
         <h2>What we offer</h2>
-        <div className='card'>
-          <h3>Hair styling</h3>
-          <p>Modern styling and smmoth finish and modern stylish look</p>
-        </div>
-        <div className='card'>
-          <h3>Skin Treatment</h3>
-          <p>Glow facials ,cleanup,and best look your skin and better experience</p>
-
-        </div>
-        <div className='card'>
-          <h3>Beard shaving</h3>
-          <p>precise trimming and beard for a sharp,clean,look </p>
-        </div>
+        
         </section> 
+       
+         
+         <div className='card-section'>
+          <div className='card'>
+            <h3>
+              <span className='color1'>Hair</span>
+              <span className='color2'>Styling</span>
+            </h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, quod, sunt error expedita eaque, unde soluta corrupti in fugiat sed tempore earum! Magni similique aliquid iste accusantium vel nostrum? Aspernatur.</p>
+            <button id='first-btn'>Learn More...</button>
+          </div>
+          <div className='card'>
+            <h3>
+              <span className='brown-clr'>Facial</span>
+              <span className='green-clr'>Treatment</span>
+            </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae sit reprehenderit nesciunt eum animi inventore, vero aperiam? Quia, sunt praesentium voluptas placeat culpa aliquam vitae natus tempora temporibus beatae!</p>
+
+            <button id='first-btn'>Learn More...</button>
+          </div>
+          <div className='card'>
+            <h3>
+              <span>Beard</span>
+              <span className='red-clr'>Grooming</span>
+             
+            </h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet beatae maxime quo repellendus corporis molestiae distinctio praesentium unde, inventore voluptate eos culpa alias ullam quae, obcaecati, doloremque ea? Sunt, in.</p>
+            <button id='first-btn'>Learn More...</button>
+          </div>
+         </div>
+          </div>
+      
      
-        </div>
-  
+     
      
       
     
